470cc87174a804714f9503d286a96a0fb50c8868
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-13 02:29:36 +0700

    comment
    kaka

---
+++ constructor.js
@@ -0,0 +1,12 @@
+//*****************WELCOME oop Java script*******************
+//visit our website more info cismox.com
+
+
+function Person()//When you make the constructor so that time must be defined the person first character wil be uppercase
+{
+	this.name='darshit';
+	this.father_name='hasmukhbhai';//this is keyword.it is used for adding the property der object
+	this.mother_name='anandiben';
+}
+var der= new Person();//der is the objetct of person constructor
+console.log(der)
