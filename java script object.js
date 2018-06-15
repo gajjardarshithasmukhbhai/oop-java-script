6fd43a6fc4c0a9736be20103f5e1ed1055ac3dd2
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-15 12:49:33 +0700

    cmmc
    mmmsm

---
+++ java script object.js
@@ -0,0 +1,35 @@
+/********************HELLO java script object******************/
+/*step 1:define the variable name 
+  step 2:then after the caliy braces({}) 
+  	ex.let a={
+	
+  }
+  step 3:then after object variable will be declare
+  	ex.let a={
+		title
+  	}
+  step 4:declare variable then after ( : )
+  	ex.let a={
+		title:'gajjar'
+  	}
+  step 5:then after seprate two variable through (,)
+  	ex.let a={
+		title:'gajajr',
+		father_name:'hasmukhbhai',
+		mother_name:'anandiben'
+  	}
+*/
+
+let a={
+	title:'gajjar darshit',
+	subjetct:'information technolgy',
+	college:'goverment engineering college bhavnagar'
+	  }
+console.log(`i am ${a.title}.i study in ${a.college} and my subject is ${a.subject}`)
+
+let cd={
+	title:'gajjar',
+	name:'hasmukhbhai',
+	father_name:'babulal'
+}
+console.log(`hi ${cd.title} ${cd.name} and my fater's name is ${cd.father_name}`);
