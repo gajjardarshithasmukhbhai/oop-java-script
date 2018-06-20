cf149ebac4b9d1243f717245fd9337a5d8a8e503
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-21 01:52:24 +0700

    snns
    nn

---
+++ Random number generator.js
@@ -0,0 +1,12 @@
+//oop java script random number generator
+console.log("Random Generator")
+
+var wer=Math.random()*10;
+if(wer<4)
+{
+	console.log(Math.ceil(wer))
+}
+else {
+	
+console.log(Math.floor(wer));
+}
