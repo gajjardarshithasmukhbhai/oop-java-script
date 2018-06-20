f189459bb0b7a08d964eefd2f71d69f37fc3b7df
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-21 01:21:17 +0700

    cjcjc

---
+++ floor and ceil.js
@@ -0,0 +1,8 @@
+//object oriented programming  floor and ceil method 
+console.log("*****floor and ceil*****")
+let a=22
+let b=7
+let c=22/7
+console.log(c.toFixed(2))
+console.log(`your value is ${Math.floor(c)}`)
+console.log(`your value is ${Math.ceil(c)}`)
