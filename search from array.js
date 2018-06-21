d97d768752d0a695721cf5708bd8760461ed7b42
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-21 17:51:37 +0700

    mmd

---
+++ search from array.js
@@ -0,0 +1,21 @@
+//object oriented java script searching property
+
+let a=["Buy bread","watch jurassic movie","startup"]
+console.log(a.indexOf('startup'))
+//output 
+
+//-->return the 0.it's mean that value will be find
+
+ /*console.log(`gajjar darshit`)
+ let array=[{
+ 		title:"searching",
+ 		name:"gajjar darshit",
+
+ },{
+
+ },{
+
+ },{
+
+ }]
+ */
