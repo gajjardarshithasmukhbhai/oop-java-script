efaf6885483231812fd6ef8458988d0bc8cb5684
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-21 18:45:11 +0700

    cmcm

--- search from array.js
+++ search from array.js
@@ -9,9 +9,11 @@ console.log(a.indexOf('startup'))
 
 let ad=["nikunj","darshit","meet up"]
 console.log(ad[ad.indexOf("darshit")])
+//output::           ^
+//-->return the  ----| memory position and then after ad[aray]-->print that
 
  /*console.log(`gajjar darshit`)
- let array=[{
+ let array=[{ 
  		title:"searching",
  		name:"gajjar darshit",
 
