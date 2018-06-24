e639a72aa64a607680707788afc5d5d4ac26b8b9
gajjardarshithasmukhbhai <darshit.gajjar1998@gmail.com>
2018-06-24 16:05:32 +0700

    mmcm
    mm

---
+++ Arrow Function.js
@@ -0,0 +1,14 @@
+//---------Normal Function--------
+console.log(`Normal Function`)
+let der=function (ferd) {
+	return `hi I am ${ferd} Owner Of cismox`
+}
+console.log(der("gajjar darshit"))
+//            ||
+//           ++++
+//            ++
+//             +
+//-----------Arrow Function-------
+console.log(`Arrow Function`)
+let gfr=(ferd)=>`hi I am ${ferd} Owner Of cismox`
+console.log(gfr("glyphicon"))
